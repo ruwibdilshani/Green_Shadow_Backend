88360d13bedd26bfb1d86d6773edb94fda362183
ruwibdilshani <ruwibdilshani0921@gmail.com>
2024-11-10 22:25:28 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/green_shadow_backend/GreenShadowBackendApplication.java
@@ -0,0 +1,19 @@
+package lk.ijse.green_shadow_backend;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GreenShadowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenShadowBackendApplication.class, args);
+    }
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+}
