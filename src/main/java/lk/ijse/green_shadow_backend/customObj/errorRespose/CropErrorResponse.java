f525c588d7dde51b097f8b7990c35f8875d4e7e0
ruwibdilshani <ruwibdilshani0921@gmail.com>
2024-11-11 21:48:09 +0530

    Crop customObj created

---
+++ src/main/java/lk/ijse/green_shadow_backend/customObj/errorRespose/CropErrorResponse.java
@@ -0,0 +1,17 @@
+package lk.ijse.green_shadow_backend.customObj.errorRespose;
+
+
+import lk.ijse.green_shadow_backend.customObj.CropResponse;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CropErrorResponse implements CropResponse, Serializable {
+    private int errorCode;
+    private String errorMessage;
+}
