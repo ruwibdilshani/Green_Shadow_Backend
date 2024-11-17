93f1fc94a5d401385b61c28ee59d6c24057aee57
ruwibdilshani <ruwibdilshani0921@gmail.com>
2024-11-17 12:29:24 +0530

    Done

---
+++ src/test/java/lk/ijse/green_shadow_backend/GreenShadowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.green_shadow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenShadowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
