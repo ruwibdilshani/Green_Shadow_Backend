a165325f529c2aaf96b344a454a42d36c7f01012
ruwibdilshani <ruwibdilshani0921@gmail.com>
2024-12-04 12:56:16 +0530

    initial commit

---
+++ src/main/java/lk/ijse/green_shadow_backend/entity/User.java
@@ -0,0 +1,60 @@
+package lk.ijse.green_shadow_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "user")
+public class User implements UserDetails {
+    @Id
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    // why using Enumerated ?
+    // Enumerated is used to map the enum type attribute in the entity class.
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        HashSet<GrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_"+role.name()));
+        return authorities;
+    }
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
