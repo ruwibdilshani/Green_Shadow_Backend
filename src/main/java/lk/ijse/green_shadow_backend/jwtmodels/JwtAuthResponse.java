47767919f999120b9717be624bacc8a76c3568b0
ruwibdilshani <ruwibdilshani0921@gmail.com>
2024-12-04 12:51:19 +0530

    JWT Done

---
+++ src/main/java/lk/ijse/green_shadow_backend/jwtmodels/JwtAuthResponse.java
@@ -0,0 +1,13 @@
+package lk.ijse.green_shadow_backend.jwtmodels;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class JwtAuthResponse {
+    private String token;
+}
---
+++ src/main/java/lk/ijse/green_shadow_backend/service/JWTService.java
@@ -0,0 +1,10 @@
+package lk.ijse.green_shadow_backend.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface JWTService {
+    String extractUsername(String token);
+    String generateToken(UserDetails userDetails);
+    String refreshToken(UserDetails userDetails);
+    boolean isTokenValid(String token, UserDetails userDetails);
+}
